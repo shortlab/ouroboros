9240b999b2cb5f53cba915bae569d3b271c9f5ad
Alicia M. Elliott <aliciae@mit.edu>
2018-02-07 16:15:45 -0500

    added PhaseField back to OuroborosApp.C

--- src/base/OuroborosApp.C
+++ src/base/OuroborosApp.C
@@ -3,7 +3,7 @@
 #include "AppFactory.h"
 #include "MooseSyntax.h"
 // #include "ModulesApp.h"
-//#include "PhaseFieldApp.h"
+#include "PhaseFieldApp.h"
 #include "MiscApp.h"
 
 #include "ReactorHeatSource.h"
@@ -32,13 +32,13 @@ OuroborosApp::OuroborosApp(InputParameters parameters) :
 {
   Moose::registerObjects(_factory);
 //  ModulesApp::registerObjects(_factory);
-//  PhaseFieldApp::registerObjects(_factory);
+  PhaseFieldApp::registerObjects(_factory);
   MiscApp::registerObjects(_factory);
   OuroborosApp::registerObjects(_factory);
 
   Moose::associateSyntax(_syntax, _action_factory);
 //  ModulesApp::associateSyntax(_syntax, _action_factory);
-  // PhaseFieldApp::associateSyntax(_syntax, _action_factory);
+  PhaseFieldApp::associateSyntax(_syntax, _action_factory);
   MiscApp::associateSyntax(_syntax, _action_factory);
   OuroborosApp::associateSyntax(_syntax, _action_factory);
 }
